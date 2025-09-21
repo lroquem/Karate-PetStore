8ec0c9d904c473130efa1587363334afbeabaddc
Luis Roque <lroquem@unsa.edu.pe>
2025-09-21 15:53:38 -0500

    test: modificar UserTestsRunner para ejecutar los features en orden específico

--- src/test/java/user/UserTestsRunner.java
+++ src/test/java/user/UserTestsRunner.java
@@ -4,7 +4,10 @@ import com.intuit.karate.Runner;
 
 public class UserTestsRunner {
     public static void main(String[] args) {
-        Runner.path("src/test/java/user")
+        Runner.path("src/test/java/user/CrearUser.feature",
+                        "src/test/java/user/ObtenerUser.feature",
+                        "src/test/java/user/ModificarUser.feature"
+                )
                 .outputCucumberJson(true)
                 .parallel(1);
     }
